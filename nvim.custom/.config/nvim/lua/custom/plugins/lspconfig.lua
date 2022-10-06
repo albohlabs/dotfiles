99f8afa6fd543b020bdf02a325406ad2e1c8848b
Daniel Pfefferkorn <github@alboh.de>
2022-10-06 20:41:24 +0200

    remove lsp_lines

--- nvim.custom/.config/nvim/lua/custom/plugins/lspconfig.lua
+++ nvim.custom/.config/nvim/lua/custom/plugins/lspconfig.lua
@@ -1,31 +1,31 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 -- disable and use lsp_lines.nvim
-vim.diagnostic.config {
-  virtual_text = false,
-}
+-- vim.diagnostic.config {
+--   virtual_text = false,
+-- }
 
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 local servers = {
-  "bashls",
-  "cssls",
-  "dockerls",
-  "emmet_ls",
-  -- "eslint",
-  -- "graphql",
-  "hls",
-  "html",
-  "jsonls",
-  "prismals",
-  -- "tailwindcss",
-  "tsserver",
+	"bashls",
+	"cssls",
+	"dockerls",
+	"emmet_ls",
+	-- "eslint",
+	-- "graphql",
+	"hls",
+	"html",
+	"jsonls",
+	"prismals",
+	-- "tailwindcss",
+	"tsserver",
 }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
