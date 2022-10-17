f52a93d4ac1dac998bc075b4ae940291ffdb61e8
Daniel Pfefferkorn <github@alboh.de>
2022-10-17 15:48:17 +0200

    change theme

--- nvim.custom/.config/nvim/lua/custom/chadrc.lua
+++ nvim.custom/.config/nvim/lua/custom/chadrc.lua
@@ -4,25 +4,25 @@
 local M = {}
 
 M.ui = {
-  theme = "nord",
-  theme_toggle = { "everforest", "one_light" },
+	theme = "onenord",
+	theme_toggle = { "onenord", "one_light" },
 }
 
-local override = require "custom.override"
+local override = require("custom.override")
 
 M.plugins = {
-  override = {
-    ["kyazdani42/nvim-tree.lua"] = override.nvimtree,
-    ["nvim-treesitter/nvim-treesitter"] = override.treesitter,
-    ["nvim-telescope/telescope.nvim"] = override.telescope,
-    ["williamboman/mason.nvim"] = override.mason,
-    ["hrsh7th/nvim-cmp"] = override.cmp,
-    ["neovim/nvim-lspconfig"] = override.lspconfig,
-  },
+	override = {
+		["kyazdani42/nvim-tree.lua"] = override.nvimtree,
+		["nvim-treesitter/nvim-treesitter"] = override.treesitter,
+		["nvim-telescope/telescope.nvim"] = override.telescope,
+		["williamboman/mason.nvim"] = override.mason,
+		["hrsh7th/nvim-cmp"] = override.cmp,
+		["neovim/nvim-lspconfig"] = override.lspconfig,
+	},
 
-  user = require "custom.plugins",
+	user = require("custom.plugins"),
 }
 
-M.mappings = require "custom.mappings"
+M.mappings = require("custom.mappings")
 
 return M
