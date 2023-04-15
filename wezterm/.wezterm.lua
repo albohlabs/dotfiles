d4363d80f722349defc379f1e7da334da41ff4a0
Daniel Pfefferkorn <github@alboh.de>
2023-04-15 15:02:32 +0200

    zsh update

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,20 +5,18 @@ return {
   -- Font config
   font = wezterm.font("Hasklug Nerd Font"),
   font_size = 12,
-  line_height = 1.2,
+  line_height = 1.25,
   warn_about_missing_glyphs = false,
   -- Cursor style
-  default_cursor_style = "BlinkingBlock",
-  -- X11
-  enable_wayland = true,
+  default_cursor_style = "BlinkingBar",
   -- color_scheme = "Catppuccin Frappe",
   color_scheme = "Catppuccin Macchiato",
   -- color_scheme = "Catppuccin Mocha",
 
   -- Padding
   window_padding = {
-    left = 2,
-    right = 2,
+    left = 0,
+    right = 0,
     top = 0,
     bottom = 0,
   },
@@ -31,5 +29,6 @@ return {
   window_close_confirmation = "NeverPrompt",
   bold_brightens_ansi_colors = true,
   window_background_opacity = 0.9,
-  cell_width = 0.9,
+  text_background_opacity = 0.8,
+  -- cell_width = 0.9,
 }
---
+++ zsh/.zshenv
@@ -0,0 +1,4 @@
+# Skip the not helping Ubuntu global compinit
+skip_global_compinit=1
+
+. "$HOME/.cargo/env"
