47b79c4118209d6047fe9163113442da8e47fd40
Daniel Pfefferkorn <github@alboh.de>
2023-04-17 10:35:37 +0200

    set vim navbuddy to full size

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -145,7 +145,14 @@ return {
       { "MunifTanjim/nui.nvim" },
     },
     event = "LspAttach",
-    opts = { lsp = { auto_attach = true } },
+    opts = {
+      lsp = { auto_attach = true },
+      window = {
+        border = "single", -- "rounded", "double", "solid", "none"
+        size = "100%", -- Or table format example: { height = "40%", width = "100%"}
+        position = "100%",
+      },
+    },
     keys = {
       { "<leader>ct", "<cmd>Navbuddy<CR>", desc = "[C]ode  [T]ree" },
     },
