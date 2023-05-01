6e7f1c8e0d52a62aee640b041d629bd1fc71cace
Daniel Pfefferkorn <github@alboh.de>
2023-05-01 13:28:29 +0200

    nvim: remove legendary

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,7 +1,3 @@
-local function legendary_open()
-  require("legendary").find()
-end
-
 return {
   {
     "goolord/alpha-nvim",
@@ -44,23 +40,6 @@ return {
         })
       end
     end,
-  },
-
-  {
-    "mrjones2014/legendary.nvim",
-    name = "legendary",
-    keys = {
-      { "<leader>k", legendary_open, desc = "Command Palette" },
-    },
-    -- sqlite is only needed if you want to use frecency sorting
-    dependencies = {
-      "kkharji/sqlite.lua",
-      "folke/which-key.nvim",
-    },
-    event = "VeryLazy",
-    opts = {
-      which_key = { auto_register = true },
-    },
   },
 
   -- floating winbar
