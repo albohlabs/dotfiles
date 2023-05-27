9064c1e44483b1041771cce56004686bc706e479
Daniel Pfefferkorn <github@alboh.de>
2023-05-27 14:04:05 +0200

    wezterm: remove color gradient

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -13,25 +13,6 @@ return {
 	color_scheme = "Catppuccin Macchiato",
 	-- color_scheme = "Catppuccin Mocha",
 
-	window_background_gradient = {
-		orientation = "Horizontal",
-		colors = {
-			-- "#0f0c29",
-			-- "#1e1e2d",
-			-- "#302b63",
-			"#00000C",
-			-- "#00003F",
-			"#000026",
-			"#00000C",
-			-- "#24243e",
-			-- "#1e1e2d",
-			-- "#0f0c29",
-		},
-		interpolation = "CatmullRom",
-		blend = "Rgb",
-		noise = 0,
-	},
-
 	-- Padding
 	window_padding = {
 		left = 0,
