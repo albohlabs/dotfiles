a81526ae609d8b145790c3b01aae4cf59a19b1b6
Daniel Pfefferkorn <github@alboh.de>
2023-06-05 11:52:37 +0200

    wezterm: fix mouse cursor hidden

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,22 +1,36 @@
 -- https://wezfurlong.org/wezterm/config/lua/config/
 local wezterm = require("wezterm")
 
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		-- color_scheme = "Catppuccin Frappe",
+		-- color_scheme = "Catppuccin Macchiato",
+		-- color_scheme = "Catppuccin Mocha",
+
+		-- color_scheme = "tokyonight_day",
+		-- color_scheme = "tokyonight_night",
+		-- color_scheme = "tokyonight_storm",
+		-- return "Catppuccin Mocha"
+		return "tokyonight_moon"
+	else
+		-- return "Catppuccin Latte"
+		-- return "tokyonight_day"
+		return "tokyonight_moon"
+	end
+end
+
 return {
 	-- Font config
 	font = wezterm.font("Hasklug Nerd Font"),
 	font_size = 12,
 	line_height = 1.25,
 	warn_about_missing_glyphs = false,
+
+	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+
 	-- Cursor style
 	default_cursor_style = "BlinkingBar",
-	-- color_scheme = "Catppuccin Frappe",
-	-- color_scheme = "Catppuccin Macchiato",
-	-- color_scheme = "Catppuccin Mocha",
-
-	color_scheme = "tokyonight_moon",
-	-- color_scheme = "tokyonight_day",
-	-- color_scheme = "tokyonight_night",
-	-- color_scheme = "tokyonight_storm",
+	xcursor_theme = "Yaru",
 
 	-- Padding
 	window_padding = {
@@ -25,6 +39,7 @@ return {
 		top = 0,
 		bottom = 0,
 	},
+
 	-- Tab Bar
 	enable_tab_bar = false,
 	hide_tab_bar_if_only_one_tab = true,
