1efd7b767192094e30b1a80e686e53ba4a1f7ebb
Daniel Pfefferkorn <github@alboh.de>
2023-06-29 10:15:08 +0200

    massive update

--- nvim/.config/nvim/lua/lvim/editor.lua
+++ nvim/.config/nvim/lua/lvim/editor.lua
@@ -185,11 +185,4 @@ return {
       },
     },
   },
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {},
-    keys = {},
-  },
 }
---
+++ nvim/.config/nvim/lua/plugins/barbecue.lua
@@ -0,0 +1,14 @@
+return {
+  "utilyre/barbecue.nvim",
+  name = "barbecue",
+  version = "*",
+  dependencies = {
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons",
+  },
+  event = {
+    "BufReadPre",
+    "BufNewFile",
+  },
+  opts = {},
+}
