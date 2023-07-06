2cdcf49461d8754c6c5b22b880d6c27a1407f86d
Daniel Pfefferkorn <github@alboh.de>
2023-07-06 10:41:29 +0200

    nvim: add mini files plugin

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.editor.flash" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "lazyvim.plugins.extras.editor.mini-files" },
 
     { import = "lvim" },
     { import = "plugins" },
