8dc16409a22218272c934430320ade37fed8829d
Daniel Pfefferkorn <github@alboh.de>
2023-07-09 20:18:32 +0200

    nvim: transparent colorscheme

--- nvim/.config/nvim/lua/lvim/colorscheme.lua
+++ nvim/.config/nvim/lua/lvim/colorscheme.lua
@@ -1,16 +1,15 @@
 return {
   {
     "tokyonight.nvim",
-    lazy = false,
     priority = 1000,
     opts = function()
       return {
         style = "moon",
-        -- transparent = true,
-        -- styles = {
-        --   sidebars = "transparent",
-        --   floats = "transparent",
-        -- },
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
         sidebars = {
           "qf",
           "vista_kind",
