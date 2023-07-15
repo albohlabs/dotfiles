c36a25bd4eec194691f7f7890c5fb745fbb249af
Daniel Pfefferkorn <github@alboh.de>
2023-07-15 15:43:06 +0200

    nvim: add some mappings

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,20 +2,19 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local opts = { noremap = true, expr = true }
+
 local util = require("utils")
 util.cowboy()
 
 -- https://neovim.io/doc/user/vimindex.html
 
--- vim.keymap.set(
---   "n",
---   "<leader>xs",
---   require("telescope.builtin").resume,
---   { noremap = true, silent = true, desc = "Resume" }
--- )
---
 vim.keymap.set("n", "<C-O>", "<C-O>zz", { noremap = true, silent = true, desc = "move back" })
 vim.keymap.set("n", "<C-I>", "<C-I>zz", { noremap = true, silent = true, desc = "move forward" })
+
+-- Center on page up/down
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
 vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
 vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
@@ -23,16 +22,18 @@ vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buff
 vim.keymap.del("n", "<S-h>")
 vim.keymap.del("n", "<S-l>")
 
+-- go out of closing bracket
+vim.keymap.set("i", "jj", "<c-o>:call search('}\\|)\\|]\\|>\\|\"', 'cW')<cr><Right>")
+
 -- smart deletion, dd
--- It solves the issue, where you want to delete empty line, but dd will override you last yank.
--- Code above will check if u are deleting empty line, if so - use black hole register.
+-- Code will check if u are deleting empty line, if so - use black hole register.
 vim.keymap.set("n", "dd", function()
   if vim.api.nvim_get_current_line():match("^%s*$") then
     return '"_dd'
   else
     return "dd"
   end
-end, { noremap = true, expr = true })
+end, opts)
 
 -- smart indent, i
 -- indent properly on empty lines
@@ -42,9 +43,9 @@ vim.keymap.set("n", "i", function()
   else
     return "i"
   end
-end, { noremap = true, expr = true })
+end, opts)
 
 -- absolute filepath of the current buffer:
 vim.keymap.set("n", "%%", function()
   vim.api.nvim_feedkeys(vim.fn.expand("%:p:h") .. "/", "c", false)
-end, { noremap = true, expr = true })
+end, opts)
