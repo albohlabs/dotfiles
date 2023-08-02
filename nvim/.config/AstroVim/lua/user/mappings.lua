7c8da29b02659215742982b7474237e4577fad42
Daniel Pfefferkorn <github@alboh.de>
2023-08-02 21:17:02 +0200

    nvim: improve example mappings

--- nvim/.config/AstroVim/lua/user/mappings.lua
+++ nvim/.config/AstroVim/lua/user/mappings.lua
@@ -7,8 +7,18 @@ return {
   -- first key is the mode
   n = {
     -- second key is the lefthand side of the map
+
+    -- navigate buffer tabs with `H` and `L`
+    -- L = {
+    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+    --   desc = "Next buffer",
+    -- },
+    -- H = {
+    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+    --   desc = "Previous buffer",
+    -- },
+
     -- mappings seen under group name "Buffer"
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
         require("astronvim.utils.status").heirline.buffer_picker(
@@ -17,7 +27,6 @@ return {
       end,
       desc = "Pick to close",
     },
-
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
