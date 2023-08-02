dc80d306faba759bfc8770c1f9dc0ceac7ade9e1
Daniel Pfefferkorn <github@alboh.de>
2023-08-02 21:21:13 +0200

    nvim: remove tokyo from incline

--- nvim/.config/AstroVim/lua/user/plugins/incline.lua
+++ nvim/.config/AstroVim/lua/user/plugins/incline.lua
@@ -3,18 +3,10 @@ return {
   "b0o/incline.nvim",
   event = "BufReadPre",
   config = function()
-    local colors = require("tokyonight.colors").setup()
     require("incline").setup {
       hide = {
         cursorline = true,
       },
-      highlight = {
-        groups = {
-          InclineNormal = { guibg = "#FC56B1", guifg = colors.black },
-          InclineNormalNC = { guifg = "#FC56B1", guibg = colors.black },
-        },
-      },
-      window = { margin = { vertical = 0, horizontal = 1 } },
       render = function(props)
         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
         local icon, color = require("nvim-web-devicons").get_icon_color(filename)
