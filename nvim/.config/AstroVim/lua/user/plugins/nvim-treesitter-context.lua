b66692d62cf0b59de67eb42df9cc4b1f82ebc5ee
Daniel Pfefferkorn <github@alboh.de>
2023-08-02 14:23:12 +0200

    nvim: remove tresitter context cause bad performance

--- nvim/.config/AstroVim/lua/user/plugins/nvim-treesitter-context.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter-context",
-  event = "BufReadPost",
-  opts = {
-    max_lines = 5,
-    mode = "topline",
-    on_attach = function() return vim.bo.filetype ~= "markdown" end,
-  },
-}
