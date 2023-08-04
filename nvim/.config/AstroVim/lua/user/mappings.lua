0339a1bad3adc1df8119a06d1bc5a55d906d510f
Daniel Pfefferkorn <github@alboh.de>
2023-08-04 11:09:42 +0200

    nvim: add nerdtree toggle cmd

--- nvim/.config/AstroVim/lua/user/mappings.lua
+++ nvim/.config/AstroVim/lua/user/mappings.lua
@@ -30,6 +30,9 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+
+    ["<leader>r"] = { ":Neotree toggle position=right git_status<cr>", desc = "Toggle Git Explorer" },
+
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
