2b72f76b5b598d14d6e5effc7bc61ebb08312579
Daniel Pfefferkorn <github@alboh.de>
2023-08-20 18:57:23 +0200

    neovim: mason update

---
+++ nvim/.config/AstroVim/lua/user/plugins/mason.lua
@@ -0,0 +1,42 @@
+-- customize mason plugins
+return {
+  -- use mason-lspconfig to configure LSP installations
+  {
+    "williamboman/mason-lspconfig.nvim",
+    -- overrides `require("mason-lspconfig").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(
+        opts.ensure_installed,
+        { "lua_ls", "tsserver", "eslint", "cssls", "custom_elements_ls" }
+      )
+    end,
+  },
+  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
+    "jay-babu/mason-null-ls.nvim",
+    -- overrides `require("mason-null-ls").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "eslint_d",
+        "jsonlint",
+        "markdowlint",
+        "fixjson",
+        "prettierd",
+        "luacheck",
+        "stylua",
+        "markdownlint",
+      })
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    -- overrides `require("mason-nvim-dap").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, { "chrome" })
+      opts.automatic_installation = true
+    end,
+  },
+}
