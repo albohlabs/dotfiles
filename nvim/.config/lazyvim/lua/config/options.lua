63eaea4eb39934bf46d5d4a20b04d873a15d8015
Daniel Pfefferkorn <github@alboh.de>
2023-10-15 20:11:08 +0200

    nvim: open splits in a more natural direction

--- nvim/.config/lazyvim/lua/config/options.lua
+++ nvim/.config/lazyvim/lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- default position
+vim.opt.scrolloff = 5
+
+-- open splits in a more natural direction
+-- https://vimtricks.com/p/open-splits-more-naturally/
+vim.opt.splitright = true
+vim.opt.splitbelow = true
