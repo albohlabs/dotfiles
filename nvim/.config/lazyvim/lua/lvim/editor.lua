35464a9a3cfcf13cc65f80944cf91c7a84b6808f
Daniel Pfefferkorn <github@alboh.de>
2023-10-29 14:49:40 +0100

    lazy: show hidden in neo-tree

--- nvim/.config/lazyvim/lua/lvim/editor.lua
+++ nvim/.config/lazyvim/lua/lvim/editor.lua
@@ -14,4 +14,17 @@ return {
       { "<leader>cs", "<cmd>AerialNavToggle<cr>", desc = "Aerial (Symbols)" },
     },
   },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_hidden = false, -- only works on Windows for hidden files/directories
+        },
+      },
+    },
+  },
 }
