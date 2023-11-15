9795a8edf74f5b6c5b90b73208a0f36ced60f40a
Daniel Pfefferkorn <github@alboh.de>
2023-11-15 09:12:04 +0100

    nvim: add config flash plugin

--- nvim/.config/lazyvim/lua/lvim/flash.lua
+++ nvim/.config/lazyvim/lua/lvim/flash.lua
@@ -1,4 +1,11 @@
 return {
   "folke/flash.nvim",
-  enabled = false,
+  enabled = true,
+  -- stylua: ignore
+  keys = {
+    { "s", false },
+    { "s", false },
+    { "<leader>j", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "<leader>k", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+  },
 }
