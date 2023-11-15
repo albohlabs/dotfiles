b7747917c16b0cedc1f8e52ecb17b6058ba04f07
Daniel Pfefferkorn <github@alboh.de>
2023-11-15 09:12:25 +0100

    nvim: twiligth again

--- nvim/.config/lazyvim/lua/lvim/ui.lua
+++ nvim/.config/lazyvim/lua/lvim/ui.lua
@@ -38,4 +38,18 @@ return {
       },
     },
   },
+
+  "folke/twilight.nvim",
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    opts = {
+      plugins = {
+        gitsigns = true,
+        tmux = true,
+        kitty = { enabled = false, font = "+2" },
+      },
+    },
+    keys = { { "<leader>Z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+  },
 }
