805f4b1c5e2f026c77222366fae19d53552aef01
Daniel Pfefferkorn <github@alboh.de>
2023-11-27 21:09:56 +0100

    lazyvim: add tsc

---
+++ nvim/.config/lazyvim/lua/plugins/tsc.lua
@@ -0,0 +1,12 @@
+return {
+  "dmmulroy/tsc.nvim",
+  lazy = false,
+  cmd = { "TSC" },
+  opts = {
+    flags = {
+      watch = true,
+    },
+    auto_start_watch_mode = true,
+    auto_close_qflist = true,
+  },
+}
