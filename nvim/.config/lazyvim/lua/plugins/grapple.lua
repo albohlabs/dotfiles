bcd11ea34be51fa9ca2dd35f8d6197c2dffc8dd1
alboh <github@alboh.de>
2024-03-03 21:59:34 +0100

    lazy: adjust grapple config

--- nvim/.config/lazyvim/lua/plugins/grapple.lua
+++ nvim/.config/lazyvim/lua/plugins/grapple.lua
@@ -4,28 +4,27 @@ local prefix = "<leader><leader>"
 return {
   {
     "cbochs/grapple.nvim",
-    cmd = { "Grapple" },
+    event = { "BufReadPost", "BufNewFile" },
+    command = "Grapple",
     dependencies = {
       { "nvim-tree/nvim-web-devicons", lazy = true },
     },
     keys = {
-      { prefix .. "a", "<cmd>Grapple tag<cr>", desc = "Grapple tag" },
-      { prefix .. "r", "<CMD>GrappleUntag<CR>", desc = "Grapple untag" },
-      -- { prefix .. "A", "<CMD>GrappleUntag<CR>", desc = "Grapple toggle" },
+      { "<leader>a", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
       { prefix .. "e", "<cmd>Grapple open_tags<cr>", desc = "Grapple menu" },
 
-      { prefix .. "<Tab>", "<cmd>Grapple cycle_forward<cr>", desc = "Grapple forward" },
-      { prefix .. "<S-Tab>", "<cmd>Grapple cycle_backward<cr>", desc = "Grapple backward" },
+      { "<leader>j", "<cmd>Grapple cycle_forward<cr>", desc = "Grapple forward" },
+      { "<leader>k", "<cmd>Grapple cycle_backward<cr>", desc = "Grapple backward" },
 
-      { "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Grapple Tag 1" },
-      { "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Grapple Tag 2" },
-      { "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Grapple Tag 3" },
-      { "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Grapple Tag 4" },
-      { "<leader>5", "<cmd>Grapple select index=5<cr>", desc = "Grapple Tag 5" },
-      { "<leader>6", "<cmd>Grapple select index=6<cr>", desc = "Grapple Tag 6" },
-      { "<leader>7", "<cmd>Grapple select index=7<cr>", desc = "Grapple Tag 7" },
-      { "<leader>8", "<cmd>Grapple select index=8<cr>", desc = "Grapple Tag 8" },
-      { "<leader>9", "<cmd>Grapple select index=9<cr>", desc = "Grapple Tag 9" },
+      { "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Grapple select 1" },
+      { "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Grapple select 2" },
+      { "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Grapple select 3" },
+      { "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Grapple select 4" },
+      { "<leader>5", "<cmd>Grapple select index=5<cr>", desc = "Grapple select 5" },
+      { "<leader>6", "<cmd>Grapple select index=6<cr>", desc = "Grapple select 6" },
+      { "<leader>7", "<cmd>Grapple select index=7<cr>", desc = "Grapple select 7" },
+      { "<leader>8", "<cmd>Grapple select index=8<cr>", desc = "Grapple select 8" },
+      { "<leader>9", "<cmd>Grapple select index=9<cr>", desc = "Grapple select 9" },
     },
   },
 }
