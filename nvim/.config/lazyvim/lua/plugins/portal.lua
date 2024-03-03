b5b4742dbfc8d33e96df396c21af16f4f6036618
alboh <github@alboh.de>
2024-03-03 22:04:49 +0100

    lazy: add portal

---
+++ nvim/.config/lazyvim/lua/plugins/portal.lua
@@ -0,0 +1,19 @@
+-- https://github.com/cbochs/portal.nvim
+return {
+  {
+    "cbochs/portal.nvim",
+    keys = {
+      { "<leader>i", "<cmd>Portal jumplist forward<cr>", desc = "Portal forward" },
+      { "<leader>o", "<cmd>Portal jumplist backward<cr>", desc = "Portal backward" },
+    },
+    opts = {
+      wrap = true,
+      select_first = true,
+      escape = {
+        ["<c-j>"] = true,
+        ["<esc>"] = true,
+        ["q"] = true,
+      },
+    },
+  },
+}
