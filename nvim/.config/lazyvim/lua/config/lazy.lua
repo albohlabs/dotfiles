8d6b8269a568a8d9d1e4516cba75eb8f18cf22af
alboh <github@alboh.de>
2024-03-20 07:57:02 +0100

    nvim: add lang plugins

--- nvim/.config/lazyvim/lua/config/lazy.lua
+++ nvim/.config/lazyvim/lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
 
     { import = "lvim" },
     { import = "plugins" },
+    { import = "plugins.lang" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
