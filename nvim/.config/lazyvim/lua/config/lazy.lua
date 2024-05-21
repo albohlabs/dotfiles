125171a8029327142f592c96f57a5a50a2362823
alboh <github@alboh.de>
2024-05-21 19:23:49 +0200

    neovim: remove language plugins

--- nvim/.config/lazyvim/lua/config/lazy.lua
+++ nvim/.config/lazyvim/lua/config/lazy.lua
@@ -25,7 +25,6 @@ require("lazy").setup({
 
     { import = "lvim" },
     { import = "plugins" },
-    { import = "plugins.lang" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
