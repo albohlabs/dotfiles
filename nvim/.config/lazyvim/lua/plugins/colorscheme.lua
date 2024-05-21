c0c3628b3ae13829692323367c410b6398ce74a6
alboh <github@alboh.de>
2024-05-21 19:25:21 +0200

    neovim: adjust colorscheme

--- nvim/.config/lazyvim/lua/plugins/colorscheme.lua
+++ nvim/.config/lazyvim/lua/plugins/colorscheme.lua
@@ -39,6 +39,20 @@ return {
           "startuptime",
           "Outline",
         },
+        on_highlights = function(hl, c)
+          hl.CursorLineNr = { fg = c.orange, bold = true }
+          -- hl.LineNr = { fg = c.orange, bold = true }
+          hl.LineNrAbove = { fg = c.fg_gutter }
+          hl.LineNrBelow = { fg = c.fg_gutter }
+          local prompt = "#2d3149"
+          hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg_dark }
+          hl.TelescopeBorder = { bg = c.bg_dark, fg = c.bg_dark }
+          hl.TelescopePromptNormal = { bg = prompt }
+          hl.TelescopePromptBorder = { bg = prompt, fg = prompt }
+          hl.TelescopePromptTitle = { bg = c.fg_gutter, fg = c.orange }
+          hl.TelescopePreviewTitle = { bg = c.bg_dark, fg = c.bg_dark }
+          hl.TelescopeResultsTitle = { bg = c.bg_dark, fg = c.bg_dark }
+        end,
       }
     end,
   },
