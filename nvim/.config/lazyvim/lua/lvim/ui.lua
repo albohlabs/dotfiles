e73a3d0614a998df83ccbcb7a586020c9dbcc2c8
alboh <github@alboh.de>
2024-06-03 07:53:49 +0200

    lazyvim: reduce noice

--- nvim/.config/lazyvim/lua/lvim/ui.lua
+++ nvim/.config/lazyvim/lua/lvim/ui.lua
@@ -4,6 +4,8 @@ return {
   {
     "folke/noice.nvim",
     opts = function(_, opts)
+      opts.debug = false
+
       table.insert(opts.routes, {
         filter = {
           event = "notify",
@@ -11,16 +13,40 @@ return {
         },
         opts = { skip = true },
       })
+      local focused = true
+      vim.api.nvim_create_autocmd("FocusGained", {
+        callback = function()
+          focused = true
+        end,
+      })
+      vim.api.nvim_create_autocmd("FocusLost", {
+        callback = function()
+          focused = false
+        end,
+      })
 
-      opts.views = {
-        mini = {
-          win_options = {
-            winblend = 0,
+      table.insert(opts.routes, 1, {
+        filter = {
+          ["not"] = {
+            event = "lsp",
+            kind = "progress",
           },
+          cond = function()
+            return not focused
+          end,
         },
-      }
+        view = "notify_send",
+        opts = { stop = false },
+      })
 
-      opts.presets.lsp_doc_border = true
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function(event)
+          vim.schedule(function()
+            require("noice.text.markdown").keys(event.buf)
+          end)
+        end,
+      })
     end,
   },
 
