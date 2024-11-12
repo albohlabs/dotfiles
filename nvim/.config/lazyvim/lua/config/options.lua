4a87e743ea988c73cfd70b92a91662ff2232905a
alboh <github@alboh.de>
2024-11-12 09:13:39 +0100

    update nvim config

--- nvim/.config/lazyvim/lua/config/options.lua
+++ nvim/.config/lazyvim/lua/config/options.lua
@@ -2,37 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- leader
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- make all keymaps silent by default
+local keymap_set = vim.keymap.set
+---@diagnostic disable-next-line: duplicate-set-field
+vim.keymap.set = function(mode, lhs, rhs, opts)
+  opts = opts or {}
+  opts.silent = opts.silent ~= false
+  return keymap_set(mode, lhs, rhs, opts)
+end
 
--- default position
-vim.opt.scrolloff = 5
-
--- open splits in a more natural direction
--- https://vimtricks.com/p/open-splits-more-naturally/
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
-vim.opt.wrap = false
-
---  Autoformatting
---  TODO: Can we get rid of concat here?
-vim.opt.formatoptions = table.concat({
-  "2", -- Use the second line's indent vale when indenting (allows indented first line)
-  "q", -- Formatting comments with `gq`
-  "w", -- Trailing whitespace indicates a paragraph
-  "j", -- Remove comment leader when makes sense (joining lines)
-  "r", -- Insert comment leader after hitting Enter
-  "o", -- Insert comment leader after hitting `o` or `O`
-})
-
--- Messages
-vim.opt.shortmess:append({
-  I = true, -- No splash screen
-  W = true, -- Don't print "written" when editing
-  a = true, -- Use abbreviations in messages ([RO] intead of [readonly])
-  c = true, -- Do not show ins-completion-menu messages (match 1 of 2)
-  F = true, -- Do not print file name when opening a file
-  s = true, -- Do not show "Search hit BOTTOM" message
-})
+-- better coop with fzf-lua
+vim.env.FZF_DEFAULT_OPTS = ""
+vim.g.ai_cmp = false
+vim.g.lazyvim_blink_main = true
--- nvim/.config/lazyvim/lua/utils.lua
+++ nvim/.config/lazyvim/lua/utils.lua
@@ -1,64 +1,26 @@
 -- https://github.com/AstroNvim/astrocommunity/blob/main/lua/astrocommunity/utils.lua
 local M = {}
 
---- Call function if a condition is met
----@param func function The function to run
----@param condition boolean # Whether to run the function or not
----@return any|nil result # the result of the function running or nil
-function M.conditional_func(func, condition, ...)
-  -- if the condition is true or no condition is provided, evaluate the function with the rest of the parameters and return the result
-  if condition and type(func) == "function" then
-    return func(...)
-  end
-end
-
---- Insert one or more values into a list like table and maintain that you do not insert non-unique values (THIS MODIFIES `lst`)
----@param lst any[]|nil The list like table that you want to insert into
----@param vals any|any[] Either a list like table of values to be inserted or a single value to be inserted
----@return any[] # The modified list like table
-function M.list_insert_unique(lst, vals)
-  if not lst then
-    lst = {}
-  end
-  assert(vim.tbl_islist(lst), "Provided table is not a list like table")
-  if not vim.tbl_islist(vals) then
-    vals = { vals }
-  end
-  local added = {}
-  vim.tbl_map(function(v)
-    added[v] = true
-  end, lst)
-  for _, val in ipairs(vals) do
-    if not added[val] then
-      table.insert(lst, val)
-      added[val] = true
-    end
-  end
-  return lst
-end
-
 function M.cowboy()
   ---@type table?
-  local id
   local ok = true
   for _, key in ipairs({ "h", "j", "k", "l", "+", "-" }) do
     local count = 0
-    local timer = assert(vim.loop.new_timer())
+    local timer = assert(vim.uv.new_timer())
     local map = key
     vim.keymap.set("n", key, function()
       if vim.v.count > 0 then
         count = 0
       end
       if count >= 10 and vim.bo.buftype ~= "nofile" then
-        ok, id = pcall(vim.notify, "Hold it Cowboy!", vim.log.levels.WARN, {
+        ok = pcall(vim.notify, "Hold it Cowboy!", vim.log.levels.WARN, {
           icon = "🤠",
-          replace = id,
+          id = "cowboy",
           keep = function()
             return count >= 10
           end,
         })
         if not ok then
-          id = nil
           return map
         end
       else
